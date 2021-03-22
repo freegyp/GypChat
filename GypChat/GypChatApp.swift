34ce6844268ab08c68c861591fb40a04e6a6bd3a
Yupeng Gu <freegyp@gmail.com>
2021-03-22 21:56:38 +0800

    Initial Commit

---
+++ GypChat/GypChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GypChatApp.swift
+//  GypChat
+//
+//  Created by Yupeng Gu on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct GypChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
